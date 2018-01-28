604e6bc00d1dad54d03be5d4560fadcc3324f135
Kevin Albrich <albkea14@htlkaindorf.at>
2018-01-28 16:29:39 +0100

    GUI hinzugefügt

--- src/main/ClientMain.java
+++ src/main/ClientMain.java
@@ -17,62 +17,69 @@ import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
 import de.lessvoid.nifty.screen.DefaultScreenController;
 
 /**
- * 
+ *
  * @author Robbo13
  */
-public class ClientMain extends SimpleApplication{
+public class ClientMain extends SimpleApplication {
 
-     private Nifty nifty;
-    
+    private Nifty nifty;
+
     public static void main(String[] args) {
 //        ClientMain clientMain = new ClientMain();
 //        clientMain.start(JmeContext.Type.Display);
-        
+
         ClientMain app = new ClientMain();
         app.start();
     }
 
     @Override
     public void simpleInitApp() {
-  NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
-            assetManager, inputManager, audioRenderer, guiViewPort);
-    Nifty nifty = niftyDisplay.getNifty();
-    guiViewPort.addProcessor(niftyDisplay);
-    flyCam.setDragToRotate(true);
+        NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+                assetManager, inputManager, audioRenderer, guiViewPort);
+        Nifty nifty = niftyDisplay.getNifty();
+        guiViewPort.addProcessor(niftyDisplay);
+        flyCam.setDragToRotate(true);
 
-    nifty.loadStyleFile("nifty-default-styles.xml");
-    nifty.loadControlFile("nifty-default-controls.xml");
+        nifty.loadStyleFile("nifty-default-styles.xml");
+        nifty.loadControlFile("nifty-default-controls.xml");
+        nifty.registerMusic("mysound", "Sounds/Musics/Main.mp3");
+        // <screen>
+        nifty.addScreen("Screen_ID", new ScreenBuilder("Hello Nifty Screen") {
+            {
+                controller(new DefaultScreenController()); // Screen properties
 
-    // <screen>
-    nifty.addScreen("Screen_ID", new ScreenBuilder("Hello Nifty Screen"){{
-        controller(new DefaultScreenController()); // Screen properties
+                // <layer>
+                layer(new LayerBuilder("Layer_ID") {
+                    {
+                        childLayoutVertical(); // layer properties, add more...
 
-        // <layer>
-        layer(new LayerBuilder("Layer_ID") {{
-            childLayoutVertical(); // layer properties, add more...
+                        // <panel>
+                        panel(new PanelBuilder("Panel_ID") {
+                            {
+                                childLayoutCenter(); // panel properties, add more...
 
-            // <panel>
-            panel(new PanelBuilder("Panel_ID") {{
-               childLayoutCenter(); // panel properties, add more...
+                                // GUI elements
+                                control(new ButtonBuilder("Button_ID", "Hello Nifty") {
+                                    {
+                                        alignCenter();
+                                        valignCenter();
+                                        height("5%");
+                                        width("15%");
+                                    }
+                                });
 
-                // GUI elements
-                control(new ButtonBuilder("Button_ID", "Hello Nifty"){{
-                    alignCenter();
-                    valignCenter();
-                    height("5%");
-                    width("15%");
-                }});
+                                //.. add more GUI elements here
+                            }
+                        });
+                        // </panel>
+                    }
+                });
+                // </layer>
+            }
+        }.build(nifty));
+        // </screen>
 
-                //.. add more GUI elements here
-
-            }});
-            // </panel>
-          }});
-        // </layer>
-      }}.build(nifty));
-    // </screen>
-
-    nifty.gotoScreen("Screen_ID"); // start the screen
+        nifty.gotoScreen("Screen_ID"); // start the screen
     }
 
     @Override
