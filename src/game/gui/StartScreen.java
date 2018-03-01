f0d36845fc3f030b14ce50def18b6d068fcfa2c6
Kevin Albrich <kevin.albrich@gmx.at>
2018-03-01 11:46:28 +0100

    GUI hat probleme

--- src/game/gui/StartScreen.java
+++ src/game/gui/StartScreen.java
@@ -5,140 +5,41 @@
  */
 package game.gui;
 
-import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
-import com.jme3.app.state.AppStateManager;
-import com.jme3.asset.AssetManager;
-import com.jme3.audio.AudioRenderer;
-import com.jme3.input.InputManager;
 import com.jme3.niftygui.NiftyJmeDisplay;
-import com.jme3.renderer.ViewPort;
 import de.lessvoid.nifty.Nifty;
-import static de.lessvoid.nifty.NiftyStopwatch.start;
 import de.lessvoid.nifty.builder.ImageBuilder;
 import de.lessvoid.nifty.builder.LayerBuilder;
 import de.lessvoid.nifty.builder.PanelBuilder;
 import de.lessvoid.nifty.builder.ScreenBuilder;
 import de.lessvoid.nifty.builder.TextBuilder;
 import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
-import de.lessvoid.nifty.controls.textfield.builder.TextFieldBuilder;
-import de.lessvoid.nifty.effects.EffectProperties;
-import de.lessvoid.nifty.effects.Falloff;
-import de.lessvoid.nifty.elements.Element;
-import de.lessvoid.nifty.loaderv2.types.RegisterSoundType;
-import de.lessvoid.nifty.render.NiftyRenderEngine;
 import de.lessvoid.nifty.screen.DefaultScreenController;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
-import de.lessvoid.nifty.spi.sound.SoundHandle;
 
 /**
  *
  * @author Robbo13
  */
-public class GUIAppState extends AbstractAppState implements ScreenController {
+public class StartScreen extends SimpleApplication implements ScreenController {
 
-    public static final String MAINMENUID = "start";
-
-    private AssetManager assetManager;
-    private InputManager inputManager;
-    private AudioRenderer audioRenderer;
-    private ViewPort guiViewPort;
-
-    private Nifty nifty;
-
-    public GUIAppState() {
-
+    public static void main(String[] args) {
+        StartScreen app = new StartScreen();
+        app.start();
     }
 
     @Override
-    public void initialize(AppStateManager stateManager, Application app) {
-        super.initialize(stateManager, app);
-        this.assetManager = app.getAssetManager();
-        this.inputManager = app.getInputManager();
-        this.audioRenderer = app.getAudioRenderer();
-        this.guiViewPort = app.getGuiViewPort();
-
+    public void simpleInitApp() {
         NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
                 assetManager, inputManager, audioRenderer, guiViewPort);
-        nifty = niftyDisplay.getNifty();
+        Nifty nifty = niftyDisplay.getNifty();
         guiViewPort.addProcessor(niftyDisplay);
-        // flyCam.setDragToRotate(true);
+        flyCam.setDragToRotate(true);
 
         nifty.loadStyleFile("nifty-default-styles.xml");
         nifty.loadControlFile("nifty-default-controls.xml");
-        nifty.registerMusic("Main", "Sounds/Musics/Main.mp3");
-
-        // <screen>
-        nifty.addScreen(MAINMENUID, new ScreenBuilder("hud") {
-            {
-                controller(new DefaultScreenController());
-
-                layer(new LayerBuilder("background") {
-                    {
-                        childLayoutCenter();
-                        //backgroundColor("#000f");
-                        // <!-- ... -->
-                    }
-                });
-
-                layer(new LayerBuilder("foreground") {
-                    {
-                        childLayoutHorizontal();
-                        //backgroundColor("#0000");
-
-                        // panel added
-                        panel(new PanelBuilder("panel_left") {
-                            {
-                                childLayoutVertical();
-                                //backgroundColor("#0f08");
-                                height("100%");
-                                width("80%");
-                                // <!-- spacer -->
-                            }
-                        });
-
-                        panel(new PanelBuilder("panel_right") {
-                            {
-                                childLayoutVertical();
-                                //backgroundColor("#00f8");
-                                height("100%");
-                                width("20%");
-
-                                panel(new PanelBuilder("panel_top_right1") {
-                                    {
-                                        childLayoutCenter();
-                                        //backgroundColor("#00f8");
-                                        height("15%");
-                                        width("100%");
-                                    }
-                                });
-
-                                panel(new PanelBuilder("panel_top_right2") {
-                                    {
-                                        childLayoutCenter();
-                                        //backgroundColor("#44f8");
-                                        height("15%");
-                                        width("100%");
-                                    }
-                                });
-
-                                panel(new PanelBuilder("panel_bot_right") {
-                                    {
-                                        childLayoutCenter();
-                                        valignCenter();
-                                        //backgroundColor("#88f8");
-                                        height("70%");
-                                        width("100%");
-                                    }
-                                });
-                            }
-                        }); // panel added
-                    }
-                });
-            }
-        }.build(nifty));
 
         nifty.addScreen("start", new ScreenBuilder("start") {
             {
@@ -147,8 +48,7 @@ public class GUIAppState extends AbstractAppState implements ScreenController {
                     {
                         childLayoutCenter();
                         //backgroundColor("#000f");
-
-                        // add image
+                        // <!-- ... -->
                         image(new ImageBuilder() {
                             {
                                 filename("Textures/Images/BritishMuseum.jpg");
@@ -157,11 +57,13 @@ public class GUIAppState extends AbstractAppState implements ScreenController {
 
                     }
                 });
+
                 layer(new LayerBuilder("foreground") {
                     {
                         childLayoutVertical();
-                        backgroundColor("#0000");
+                        //backgroundColor("#0000");
 
+                        // panel added
                         // panel added
                         panel(new PanelBuilder("panel_top") {
                             {
@@ -219,15 +121,13 @@ public class GUIAppState extends AbstractAppState implements ScreenController {
                                         height("50%");
                                         width("50%");
 
-                                        // add control
                                         control(new ButtonBuilder("StartButton", "Start") {
                                             {
                                                 alignCenter();
                                                 valignCenter();
                                                 height("50%");
                                                 width("50%");
-                                                visibleToMouse(true);
-                                                interactOnClick("startGame(hud)");
+                                                interactOnClick("System.out.println(\"Hallo\")");
                                             }
                                         });
 
@@ -242,15 +142,12 @@ public class GUIAppState extends AbstractAppState implements ScreenController {
                                         height("50%");
                                         width("50%");
 
-                                        // add control
                                         control(new ButtonBuilder("QuitButton", "Quit") {
                                             {
                                                 alignCenter();
                                                 valignCenter();
                                                 height("50%");
                                                 width("50%");
-                                                visibleToMouse(true);
-                                                interactOnClick("quitGame()");
                                             }
                                         });
 
@@ -263,51 +160,106 @@ public class GUIAppState extends AbstractAppState implements ScreenController {
 
             }
         }.build(nifty));
-        // </screen>
-        nifty.gotoScreen(MAINMENUID);
-    }
 
-    @Override
+        nifty.addScreen("hud", new ScreenBuilder("hud") {
+            {
+                controller(new DefaultScreenController());
 
-    public void update(float tpf) {
+                layer(new LayerBuilder("background") {
+                    {
+                        childLayoutCenter();
+                        //backgroundColor("#000f");
+                        // <!-- ... -->
+                        image(new ImageBuilder() {
+                            {
+                                filename("Textures/Images/BritishMuseum.jpg");
+                            }
+                        });
+                    }
+                });
 
-    }
+                layer(new LayerBuilder("foreground") {
+                    {
+                        childLayoutHorizontal();
+                        //backgroundColor("#0000");
 
-    @Override
-    public void cleanup() {
-        super.cleanup();
-        //TODO: clean up what you initialized in the initialize method,
-        //e.g. remove all spatials from rootNode
-        //this is called on the OpenGL thread after the AppState has been detached
-    }
+                        // panel added
+                        panel(new PanelBuilder("panel_left") {
+                            {
+                                childLayoutVertical();
+                                //backgroundColor("#0f08");
+                                height("100%");
+                                width("80%");
+                                // <!-- spacer -->
+                            }
+                        });
 
-    /**
-     * Changes the Screen to Screen with ID screenID
-     *
-     * @param screenID the screenID
-     */
-    public void gotoScreen(String screenID) {
-        nifty.gotoScreen(screenID);
+                        panel(new PanelBuilder("panel_right") {
+                            {
+                                childLayoutVertical();
+                                //backgroundColor("#00f8");
+                                height("100%");
+                                width("20%");
+
+                                panel(new PanelBuilder("panel_top_right1") {
+                                    {
+                                        childLayoutCenter();
+                                        //backgroundColor("#00f8");
+                                        height("15%");
+                                        width("100%");
+                                    }
+                                });
+
+                                panel(new PanelBuilder("panel_top_right2") {
+                                    {
+                                        childLayoutCenter();
+                                        //backgroundColor("#44f8");
+                                        height("15%");
+                                        width("100%");
+
+                                        // add image
+                                        image(new ImageBuilder() {
+                                            {
+                                                //filename("Interface/tutorial/face1.png");
+                                                valignCenter();
+                                                alignCenter();
+                                                height("50%");
+                                                width("30%");
+                                            }
+                                        });
+
+                                    }
+                                });
+
+                                panel(new PanelBuilder("panel_bot_right") {
+                                    {
+                                        childLayoutCenter();
+                                        valignCenter();
+                                        //backgroundColor("#88f8");
+                                        height("70%");
+                                        width("100%");
+                                    }
+                                });
+                            }
+                        }); // panel added
+                    }
+                });
+            }
+        }.build(nifty));
+
+        nifty.gotoScreen("start");
     }
 
     @Override
     public void bind(Nifty nifty, Screen screen) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onStartScreen() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void startGame(String nextScreen) {
-        nifty.gotoScreen(nextScreen);  // switch to another screen
-        // start the game and do some more stuff...
     }
 
     @Override
     public void onEndScreen() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
